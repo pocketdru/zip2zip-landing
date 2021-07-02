a7dfc91561fa267664609c97aa2265a2a26fb125
pocketdru <pocketdru@gmail.com>
2021-07-02 17:46:55 -0600

    home page created and exported

---
+++ client/src/pages/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Nav from "../components/Nav";
+// import Jumbotron from "../components/Jumbotron";
+// import Footer from "../components/Footer";
+
+class Home extends Component {
+    render() {
+        return (
+        <div>
+            <Nav/>
+            {/* <Jumbotron/> */}
+            {/* <Footer /> */}
+        </div>
+        ) 
+    }
+}
+
+  export default Home;
