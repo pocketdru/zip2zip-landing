81122f0ed78fbed299c9772ddf640be3e3f3ba75
pocketdru <pocketdru@gmail.com>
2021-07-04 00:32:53 -0600

    navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./style.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 function Nav() {
   return (
@@ -9,15 +10,19 @@ function Nav() {
     <div className="container">
       <div className="row">
         <div className="col-6 col-sm-6 col-md-3 col-lg-3 offset-4 offset-sm-4 offset-md-0 offset-lg-0">
-          <a className="navbar-brand" href="/">
-              <img src="/images/nav/logo.png" width="50" height="50"  className="d-block align-top" alt="" loading="lazy"/>
-          </a>
-          <a className="flex-sm-fill pt-2 pb-2" href="/">Top One</a>
+          <div className="navbar-brand position-absolute p-4" href="/">
+              <img src="/images/nav/logo.png" width="50" height="50"  className="d-block align-top m-auto" alt="" loading="lazy"/>
+              <a className="flex-sm-fill pt-2 pb-2 d-block" href="/">Top One</a>
+          </div>
         </div>
       <div className="col-md-9 col-lg-9">
         <nav className="nav navbar d-flex justify-content-end">
-          <a className="nav-link text-sm-center" target="_blank" href="https://www.linkedin.com/company/zip-2-zip">Linkedin</a>
-          <a className="nav-link text-sm-center" target="_blank" href="/">Facebook</a>
+          <a className="nav-link text-sm-center" target="_blank" href="https://www.linkedin.com/company/zip-2-zip">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+          <a className="nav-link text-sm-center" target="_blank" href="https://www.linkedin.com/company/zip-2-zip">
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
           </nav>
       </div>
       </div>
