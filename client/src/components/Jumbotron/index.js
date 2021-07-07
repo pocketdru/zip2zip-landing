92422f684585621dc3869ade30c3ccc13bea8576
pocketdru <pocketdru@gmail.com>
2021-07-06 23:37:06 -0600

    jumbotron fixed

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,27 +1,28 @@
 import React from "react";
 import "./style.css";
+import background from "../../assets/images/jumbotron/1.jpeg"
 
 function Jumbotron() {
   return (
     <section className="jumbotron-carousel">
       <div className="container-fluid p-0">
           <div className="jumbotron jumbotron-fluid p-0">
-            <img className="d-block w-100" src="/images/jumbotron/pexels-photoscom-93398.jpeg" alt="board"/>
+            <img className="d-block w-100" src={background} alt="board"/>
             <div className="container">
       <div className="row">
         <div className="col-6 col-sm-6 col-md-3 col-lg-3 offset-4 offset-sm-4 offset-md-0 offset-lg-0">
           <div className="navbar-brand position-absolute p-4" href="/">
-              <img src="/images/nav/logo.png" width="50" height="50"  className="d-block align-top ml-auto mr-auto mt-3" alt="" loading="lazy"/>
               <a className="flex-sm-fill pt-2 pb-2 d-block" href="/">Top One</a>
           </div>
         </div>
       <div className="col-md-9 col-lg-9">
         <nav className="nav navbar d-flex justify-content-end">
-          <a className="nav-link text-sm-center" target="_blank" href="https://www.linkedin.com/company/zip-2-zip">
+          <a className="nav-link text-sm-center" href="#jumbotron">
+              Home
           </a>
-          <a className="nav-link text-sm-center" target="_blank" href="https://www.linkedin.com/company/zip-2-zip">
+          <a className="nav-link text-sm-center" href="https://www.linkedin.com/company/zip-2-zip">
           </a>
-          </nav>
+        </nav>
       </div>
       </div>
     </div> 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import logo from "../../assets/images/nav/logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
@@ -11,7 +12,7 @@ function Nav() {
       <div className="row">
         <div className="col-6 col-sm-6 col-md-3 col-lg-3 offset-4 offset-sm-4 offset-md-0 offset-lg-0">
           <div className="navbar-brand position-absolute p-4" href="/">
-              <img src="/images/nav/logo.png" width="50" height="50"  className="d-block align-top ml-auto mr-auto mt-3" alt="" loading="lazy"/>
+              <img src={logo} width="50" height="50"  className="d-block align-top ml-auto mr-auto mt-3" alt="" loading="lazy"/>
               <a className="flex-sm-fill pt-2 pb-2 d-block" href="/">Top One</a>
           </div>
         </div>
