fbc77a3426eefca758a516c67783704af0af60e4
pocketdru <pocketdru@gmail.com>
2021-07-29 20:05:35 -0600

    mini gallery grid

--- client/src/components/GalleryMini/index.js
+++ client/src/components/GalleryMini/index.js
@@ -4,7 +4,26 @@ import "./style.css";
 function GalleryMini () {
     return (
         <section className="galleryMini">
-
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                    
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card-deck">
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap"/>
+                    </div>
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap"/>
+                    </div>
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap"/>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
         </section>
     )
 }
