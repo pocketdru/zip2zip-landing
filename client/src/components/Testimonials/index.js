b424f5eda855cad149e5bc0bc5272adc1010a9c0
pocketdru <pocketdru@gmail.com>
2021-08-07 20:26:35 -0600

    testimonials polished

--- client/src/components/Testimonials/index.js
+++ client/src/components/Testimonials/index.js
@@ -7,12 +7,12 @@ function Testimonials () {
     return (
         <section className="testimonials">
             <div className="container">
-                <div className="row card-deck">
+                <div className="row card-deck pt-5 pb-5">
                     <div className="card">
                         <div className="card-body">
                             <p className="card-text">We use them every year for transporting our vehicles. Always on time and never any hassle. <span className="font-weight-bold">Great</span> group to work with.</p>
                         </div>
-                        <div className="card-footer d-flex justify-content-between">
+                        <div className="card-footer pt-0 d-flex justify-content-between">
                             <div>
                                 <p className="card-text font-weight-bold">Mark H</p>
                             </div>
@@ -24,7 +24,7 @@ function Testimonials () {
                             <p className="card-text"><span className="font-weight-bold">Positive:</span><span className="font-italic"> Professionalism, Punctuality, Quality</span><br/>
                             Great costumer service! Keep up the good work!</p>
                         </div>
-                        <div className="card-footer d-flex justify-content-between">
+                        <div className="card-footer pt-0 d-flex justify-content-between">
                             <div>
                                 <p className="card-text font-weight-bold">Tempus Logix</p>
                             </div>
@@ -35,7 +35,7 @@ function Testimonials () {
                         <div className="card-body">
                             <p className="card-text">Great and friendly company everything was very  professionally taken care off...</p>
                         </div>
-                        <div className="card-footer d-flex justify-content-between">
+                        <div className="card-footer pt-0 d-flex justify-content-between">
                             <div>
                                 <p className="card-text font-weight-bold">Dima P</p>
                             </div>
@@ -46,7 +46,7 @@ function Testimonials () {
                         <div className="card-body">
                             <p className="card-text">Very reliable company. Alex the driver is punctual and professional. A+ for the company and services they provided.I will definitely have business with Top One again!</p>
                         </div>
-                        <div className="card-footer d-flex justify-content-between">
+                        <div className="card-footer pt-0 d-flex justify-content-between">
                             <div>
                                 <p className="card-text font-weight-bold">Maxim G</p>
                             </div>
