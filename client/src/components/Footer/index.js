35d4fb6cb8d868b46548203aff447e345d6c9af3
pocketdru <pocketdru@gmail.com>
2021-08-08 22:22:16 -0600

    footer created and exported

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <section className="footer">
+            <div className="container-fluid">
+                <div className="row">
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
