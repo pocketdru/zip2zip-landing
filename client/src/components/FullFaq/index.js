9f66cf0bf32758953e1fb3e6d8eb8c4ce5f3e872
pocketdru <pocketdru@gmail.com>
2021-08-14 13:58:17 -0600

    fullfaq created and exported

---
+++ client/src/components/FullFaq/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function FullFaq() {
+    return (
+        <section className="fullFaq">
+            <div className="container">
+
+            </div>
+        </section>
+    )
+}
+
+export default FullFaq;
--- client/src/pages/FaqPage.js
+++ client/src/pages/FaqPage.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Nav from "../components/Nav";
-
+import FullFaq from "../components/FullFaq";
 import Footer from "../components/Footer";
 class FaqPage extends Component {
     render() {
         return (
         <div>
             <Nav/>
-
+            <FullFaq />
             <Footer />
         </div>
         ) 
