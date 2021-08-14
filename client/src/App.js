064b3cfa957680b5dffddd0a96ac39ab6a4d23c1
pocketdru <pocketdru@gmail.com>
2021-08-14 13:54:16 -0600

    faq page created and exported

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from "react";
 import { BrowserRouter, Route, Switch  } from "react-router-dom";
 import './fonts.css';
 import './main.css';
-import Home from "./pages";;
+import Home from "./pages";
+import FaqPage from "./pages/FaqPage.js";
 
 class App extends Component {
 
@@ -12,6 +13,9 @@ class App extends Component {
       <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route exact path="/faq">
+          <FaqPage />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ client/src/pages/FaqPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Nav from "../components/Nav";
+
+import Footer from "../components/Footer";
+class FaqPage extends Component {
+    render() {
+        return (
+        <div>
+            <Nav/>
+
+            <Footer />
+        </div>
+        ) 
+    }
+}
+
+  export default FaqPage;
