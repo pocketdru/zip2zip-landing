e2892f95bff640cee4033b9bef48467ba8f86800
pocketdru <pocketdru@gmail.com>
2021-08-15 16:56:02 -0600

    full faq created and exported

---
+++ client/src/components/FullFaq/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function FullFaq() {
+    return (
+        <section className="fullFaq pb-5">
+            <div className="container pt-5 pb-5">
+                <div className="row">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default FullFaq;
--- client/src/components/SiteMap/index.js
+++ client/src/components/SiteMap/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import FullFaq from "../FullFaq";
 import Contact from "../Contact";
 
 function SiteMap() {
@@ -7,7 +8,9 @@ function SiteMap() {
         <section className="siteMap pb-5">
             <div className="container pt-5 pb-5">
                 <div className="row">
-                    <div className="col-md-9"></div>
+                    <div className="col-md-9">
+                        <FullFaq />
+                    </div>
                     <div className="col-md-3" id="contact">
                         <Contact />
                     </div>
