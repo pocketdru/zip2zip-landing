5d468d9c8e0ea03268f13bdc928b51de3e37e9c1
pocketdru <pocketdru@gmail.com>
2021-08-23 00:07:59 -0600

    quotes

--- client/src/components/Email/index.js
+++ client/src/components/Email/index.js
@@ -20,15 +20,15 @@ for (var i = 2022; i > 1899; i--) {
     yearOptions.push((i));
 }
 
-var cars = [
-  {make: "Acura", model: ["ILX", "MDX", "NSX", "RDX", "RLX", "TLX"]},
-  {make: "Alfa Romeo", model: ["4C", "Giulia", "Giulia Quadrifoglio", "Stelvio", "Stelvio Quadrifoglio"]},
-  {make: "Aston Martin", model: ["DB11", "DBS", "Vantage"]},
-  {make: "Audi", model: ["A4", "A4 allroad", "A5", "A5 Sportback", "A6", "A6 allroad", "A7", "A8 L", "e-tron", "Q3", "Q5", "Q5 Sportback", "Q7", "Q8", "R8", "RS 5", "RS 5 Sportback", "RS 6", "RS 7", "RS Q8", "S4", "S5", "S5 sportback", "S6", "S7", "S8", "SQ5", "SQ5 Sportback", "SQ7", "SQ8", "TT", "TT RS", "TTS"]},
-  {make: "Bentley", model: ["Bentayga", "Continental", "Flying Spur", "Mulsanne"]},
+// var cars = [
+//   {make: "Acura", model: ["ILX", "MDX", "NSX", "RDX", "RLX", "TLX"]},
+//   {make: "Alfa Romeo", model: ["4C", "Giulia", "Giulia Quadrifoglio", "Stelvio", "Stelvio Quadrifoglio"]},
+//   {make: "Aston Martin", model: ["DB11", "DBS", "Vantage"]},
+//   {make: "Audi", model: ["A4", "A4 allroad", "A5", "A5 Sportback", "A6", "A6 allroad", "A7", "A8 L", "e-tron", "Q3", "Q5", "Q5 Sportback", "Q7", "Q8", "R8", "RS 5", "RS 5 Sportback", "RS 6", "RS 7", "RS Q8", "S4", "S5", "S5 sportback", "S6", "S7", "S8", "SQ5", "SQ5 Sportback", "SQ7", "SQ8", "TT", "TT RS", "TTS"]},
+//   {make: "Bentley", model: ["Bentayga", "Continental", "Flying Spur", "Mulsanne"]},
 
   
-]
+// ]
   return (
     <section id="email">
       <div className="container pt-5 pb-5">
