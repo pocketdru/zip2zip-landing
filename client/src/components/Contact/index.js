c37550111280418fd93b0302fff008f59bfce907
pocketdru <pocketdru@gmail.com>
2021-08-31 23:14:07 -0600

    mini gallery is responsive

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -6,41 +6,40 @@ import Email from '../../assets/images/contact/Layer_5.png';
 
 function Contact () {
     return (
-                        <div className="card-deck">
-                    <div className="card">
-                        <div className="card-body">
-                        <img src={Hours} className="img-fluid rounded-start" alt="..."/>
-
-                            <ul className="list-group">
-                                <li className="list-group-item" id="header">OPENING HOURS</li>
-                                <li className="list-group-item">Monday - Friday 08.00 - 5.00</li>
-                            </ul>
-                        </div>
-                        </div> 
-                        <div className="card">
-                                <div className="card-body">
-                                <img src={Call} className="img-fluid rounded-start" alt="..."/>
-                                <ul className="list-group">
-                                    <li className="list-group-item" id="header">CALL US ANYTIME</li>
-                                    <li className="list-group-item">+1 (720) 508 4966</li>
-                                    <li className="list-group-item">+1 (303) 353 8362</li>
-                                </ul>
-                        </div>                     
-                    </div>
-                    <div className="card">
-                                <div className="card-body">
-                                <img src={Email} className="img-fluid rounded-start" alt="..."/>
-                                <ul className="list-group">
-                                    <li className="list-group-item" id="header">EMAIL US</li>
-                                    <li className="list-group-item">toponetransportation@gmail.com</li>
-                                    <li className="list-group-item">toponetransportation@gmail.com</li>
-                                </ul>
-                        </div>                    
-                    </div>
-                    <div className="card d-flex align-self-center">
-                        <button type="button" className="btn m-auto">Request a quote</button>
-                    </div>
-                </div>
+    <div className="card-deck">
+        <div className="card">
+            <div className="card-body">
+                <img src={Hours} className="img-fluid rounded-start" alt="..."/>
+                <ul className="list-group">
+                    <li className="list-group-item" id="header">OPENING HOURS</li>
+                    <li className="list-group-item">Monday - Friday 08.00 - 5.00</li>
+                </ul>
+            </div>
+        </div> 
+        <div className="card">
+            <div className="card-body">
+                <img src={Call} className="img-fluid rounded-start" alt="..."/>
+                <ul className="list-group">
+                    <li className="list-group-item" id="header">CALL US ANYTIME</li>
+                    <li className="list-group-item">+1 (720) 508 4966</li>
+                    <li className="list-group-item">+1 (303) 353 8362</li>
+                </ul>
+            </div>                     
+        </div>
+        <div className="card">
+            <div className="card-body">
+                <img src={Email} className="img-fluid rounded-start" alt="..."/>
+                <ul className="list-group">
+                    <li className="list-group-item" id="header">EMAIL US</li>
+                    <li className="list-group-item">toponetransportation@gmail.com</li>
+                    <li className="list-group-item">toponetransportation@gmail.com</li>
+                </ul>
+            </div>                    
+        </div>
+        <div className="card d-flex align-self-center">
+            <button type="button" className="btn m-auto">Request a quote</button>
+        </div>
+    </div>
     )
 }
 
