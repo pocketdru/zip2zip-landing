4945ce64e1759acad88e3f64984598e7f5fb6c23
pocketdru <pocketdru@gmail.com>
2021-09-02 22:40:38 -0600

    breadcrumb created and exported

--- client/src/components/SiteMap/index.js
+++ client/src/components/SiteMap/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import FullFaq from "../FullFaq";
 import Contact from "../Contact";
+import BreadCrumbs from "../BreadCrumbs";
 
 function SiteMap() {
     return (
@@ -9,6 +10,9 @@ function SiteMap() {
             <div className="container pt-5 pb-5">
                 <div className="row">
                     <div className="col-md-9">
+                        <div className="col-md-12">
+                            <BreadCrumbs />
+                        </div>
                         <FullFaq />
                     </div>
                     <div className="col-md-3" id="contact">
