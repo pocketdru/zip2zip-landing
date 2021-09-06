67fe5ead9cb66373d9d30d16b0494d7acc158171
unknown <pocketdru@gmail.com>
2021-09-06 11:54:09 -0600

    deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cors = require('cors')
 
 app.use(cors()) // Use this after the variable declaration
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://boiling-badlands-51524.herokuapp.com"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
