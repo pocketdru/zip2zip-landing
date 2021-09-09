563b01fdb8b89aa75ad789312a33cde82531d053
pocketdru <pocketdru@gmail.com>
2021-09-09 15:37:39 -0600

    footer responsive

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,30 +1,33 @@
 import React from "react";
 import "./style.css";
-import logo from "../../assets/images/nav/logo.png";
-
 function Footer() {
     return (
         <section className="footer">
             <div className="container">
                 <div className="row align-items-end">
-                    <div className="col-md-4 d-flex align-self-center justify-content-start">
-                        <img src={logo} className="card-img-top" alt="logo" />
-                    </div> 
-                    <div className="col-md-4">
+                <div className="col-md-3">
                         <ul className="nav flex-column">
                             <li className="nav-item">
-                                <a className="nav-link active" href="/faq#left-tabs-tab-pricing">FAQs</a>
+                                <a className="nav-link active" href="/#email">Request a quote</a>
                             </li>
+                      </ul> 
+                    </div>
+                    <div className="col-md-3">
+                        <ul className="nav flex-column">
                             <li className="nav-item">
-                                <a className="nav-link active" href="#email">Request a quote</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#contact">Contact us</a>
+                                <a className="nav-link" href="/#contact">Contact us</a>
                             </li>
 
                         </ul> 
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-3">
+                        <ul className="nav flex-column">
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/faq#left-tabs-tab-pricing">FAQs</a>
+                            </li>
+                        </ul> 
+                    </div>
+                    <div className="col-md-3">
                         <ul className="nav flex-column">
                             <li className="nav-item align-text-bottom">&copy; 2021 Top One Transportation</li>
                         </ul>                   
@@ -34,5 +37,4 @@ function Footer() {
         </section>
     )
 }
-
 export default Footer;
