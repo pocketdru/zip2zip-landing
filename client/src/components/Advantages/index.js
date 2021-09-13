7f541b8eae61f5026fe1caa70e94f04d603ffb87
pocketdru <pocketdru@gmail.com>
2021-09-12 18:39:08 -0600

    advantages two more cards

--- client/src/components/Advantages/index.js
+++ client/src/components/Advantages/index.js
@@ -4,6 +4,9 @@ import Car from '../../assets/images/advantages/car-insurance.png';
 import Money from '../../assets/images/advantages/money-saving.png';
 import Map from '../../assets/images/advantages/road-map.png';
 import Rating from '../../assets/images/advantages/star-rating.png';
+import Us from '../../assets/images/advantages/about-us.png';
+import Personal from '../../assets/images/advantages/young.png';
+
 
 function Advantages () {
         return (
@@ -49,7 +52,7 @@ function Advantages () {
                             <div className="col-md-8">
                             <div className="card-body">
                                 <h5 className="card-title">Complete Vehicle Coverage</h5>
-                                <p className="card-text">We offer the most comprehensive coverage options for complete peace of mind.</p>
+                                <p className="card-text">Our selected auto haulers must meet insurance standards before qualifying to be part of our carrier network.</p>
                             </div>
                             </div>
                         </div>
@@ -65,6 +68,36 @@ function Advantages () {
                         <div className="card-body">
                             <h5 className="card-title">No Up-Front Deposit Required</h5>
                             <p className="card-text">You don't pay a penny until the day your shipment is scheduled for pickup.</p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col">
+                <div className="card mb-3">
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                        <img src={Us} className="img-fluid rounded-start" alt="money"/>
+                        </div>
+                        <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">Leave it to the pros</h5>
+                            <p className="card-text">Your car will get from point A to point B without you having to break a single sweat. By having your car delivered straight to your door, you’re allowing the experts to handle every step of the process and ensure there are no speed bumps along the way.</p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col">
+                <div className="card mb-3">
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                        <img src={Personal} className="img-fluid rounded-start" alt="money"/>
+                        </div>
+                        <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">Personalized approach</h5>
+                            <p className="card-text">Why choose our company? Top One Transportation works with you through every step of your car or truck’s move. Experience hassle-free shipping when you choose us as your car transporter and enjoy an easy and painless shipment process.</p>
                         </div>
                         </div>
                     </div>
